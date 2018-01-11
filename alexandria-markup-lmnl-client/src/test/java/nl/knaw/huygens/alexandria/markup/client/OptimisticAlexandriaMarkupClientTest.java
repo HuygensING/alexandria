35431bad39f119b22fb084bc5b941a597b7e9cdb
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2018-01-11 10:41:39 +0100

    Update Copyright headers

--- alexandria-markup-lmnl-client/src/test/java/nl/knaw/huygens/alexandria/markup/client/OptimisticAlexandriaMarkupClientTest.java
+++ alexandria-markup-lmnl-client/src/test/java/nl/knaw/huygens/alexandria/markup/client/OptimisticAlexandriaMarkupClientTest.java
@@ -1,5 +1,25 @@
 package nl.knaw.huygens.alexandria.markup.client;
 
+/*
+ * #%L
+ * alexandria-markup-lmnl-client
+ * =======
+ * Copyright (C) 2015 - 2018 Huygens ING (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import static java.util.stream.Collectors.joining;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,26 +39,6 @@ import org.junit.Test;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import nl.knaw.huygens.alexandria.markup.api.AboutInfo;
-
-/*
- * #%L
- * alexandria-markup-lmnl-client
- * =======
- * Copyright (C) 2015 - 2017 Huygens ING (KNAW)
- * =======
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 
 public class OptimisticAlexandriaMarkupClientTest extends AlexandriaTestWithTestMarkupServer {
   // private final Logger LOG = LoggerFactory.getLogger(getClass());
