054cdd00b6ba234f3b7e4a4cc04672c3da45f73d
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2018-01-11 17:49:07 +0100

    [NLA-420] Setup command line commands

---
+++ alexandria-markup-lmnl-server/src/main/java/nl/knaw/huygens/alexandria/dropwizard/cli/CheckOutCommand.java
@@ -0,0 +1,56 @@
+package nl.knaw.huygens.alexandria.dropwizard.cli;
+
+/*
+ * #%L
+ * alexandria-markup-lmnl-server
+ * =======
+ * Copyright (C) 2015 - 2018 Huygens ING (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import io.dropwizard.cli.Command;
+import io.dropwizard.setup.Bootstrap;
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
+
+public class CheckOutCommand extends Command {
+  private static final String VIEW = "view";
+  private static final String DOCUMENT = "document";
+
+  public CheckOutCommand() {
+    super("checkout", "Check out a view on a document into a file for editing.");
+  }
+
+  @Override
+  public void configure(Subparser subparser) {
+    subparser.addArgument("-v", "--view")//
+        .dest(VIEW)//
+        .type(String.class)//
+        .required(true)//
+        .help("The name of the view to use");
+
+    subparser.addArgument("-d", "--document")//
+        .dest(DOCUMENT)//
+        .type(String.class)//
+        .required(true)//
+        .help("The name of the document to view.");
+  }
+
+  @Override
+  public void run(Bootstrap<?> bootstrap, Namespace namespace) throws Exception {
+    System.out.println("Exporting document " + namespace.getString(DOCUMENT) + " using view " + namespace.getString(VIEW) + "...");
+    System.out.println("TODO");
+  }
+}
---
+++ alexandria-markup-lmnl-server/src/main/java/nl/knaw/huygens/alexandria/dropwizard/cli/RegisterDocumentCommand.java
@@ -0,0 +1,56 @@
+package nl.knaw.huygens.alexandria.dropwizard.cli;
+
+/*
+ * #%L
+ * alexandria-markup-lmnl-server
+ * =======
+ * Copyright (C) 2015 - 2018 Huygens ING (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import io.dropwizard.cli.Command;
+import io.dropwizard.setup.Bootstrap;
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
+
+public class RegisterDocumentCommand extends Command {
+  private String NAME = "name";
+  private String FILE = "file";
+
+  public RegisterDocumentCommand() {
+    super("register-document", "Parse a document and store it as TAG");
+  }
+
+  @Override
+  public void configure(Subparser subparser) {
+    subparser.addArgument("-n", "--name")//
+        .dest(NAME)//
+        .type(String.class)//
+        .required(true)//
+        .help("The name of the document");
+
+    subparser.addArgument("-f", "--file")//
+        .dest(FILE)//
+        .type(String.class)//
+        .required(true)//
+        .help("The file containing the document source");
+  }
+
+  @Override
+  public void run(Bootstrap<?> bootstrap, Namespace namespace) throws Exception {
+    System.out.println("Parsing " + namespace.getString(FILE) + " to document " + namespace.getString(NAME) + "...");
+    System.out.println("TODO");
+  }
+}
