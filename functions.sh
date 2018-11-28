c6afa6f1093e2dd0349000bd1c2dd672a276fb94
ellib <elli.bleeker@gmail.com>
2018-11-28 18:09:55 +0100

    updated tutorial and workflow diagram

--- functions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-function alexandria {
-  java -jar c:/lib/alexandria.jar $* |sed -e "s/java -jar alexandria.jar/alexandria/";
-}
-
-function alexandria-rebuild {
-  (cd ~/workspaces/alexandria-markup/ && mvn install && cd ~/workspaces/alexandria-markup-app/ && mvn package && cp alexandria-markup-server/target/alexandria-markup-server-2.0-SNAPSHOT.jar /c/lib/alexandria.jar;)
-}
