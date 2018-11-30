bb13b000de90ea3d6a27f24822a7e9aaff234092
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2018-11-30 15:20:25 +0100

    [RD-172] refactor ExportXmlCommand

---
+++ alexandria-markup-client/src/test/java/nl/knaw/huygens/alexandria/dropwizard/cli/ExportXmlCommandIntegrationTest.java
@@ -0,0 +1,89 @@
+package nl.knaw.huygens.alexandria.dropwizard.cli;
+
+/*-
+ * #%L
+ * alexandria-markup-client
+ * =======
+ * Copyright (C) 2015 - 2018 Huygens ING (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import nl.knaw.huygens.alexandria.dropwizard.cli.commands.ExportXmlCommand;
+import org.junit.Test;
+
+public class ExportXmlCommandIntegrationTest extends CommandIntegrationTest {
+
+  private static final String command = new ExportXmlCommand().getName();
+
+  @Test
+  public void testCommand() throws Exception {
+    runInitCommand();
+
+    String tagFilename = "transcriptions/transcription.tagml";
+    String tagml = "[tagml>[l>test<l]<tagml]";
+    createFile(tagFilename, tagml);
+
+    runAddCommand(tagFilename);
+    runCommitAllCommand();
+
+    boolean success = cli.run(command, "transcription");
+    assertSucceedsWithExpectedStdout(success, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+        "<xml>\n" +
+        "<tagml><l>test</l></tagml>\n" +
+        "</xml>");
+  }
+
+  @Test
+  public void testCommandInView() throws Exception {
+    runInitCommand();
+
+    String tagFilename = "transcriptions/transcription.tagml";
+    String tagml = "[tagml>[l>test<l]<tagml]";
+    createFile(tagFilename, tagml);
+    String viewName = "l";
+    String viewFilename = "views/" + viewName + ".json";
+    createFile(viewFilename, "{\"includeMarkup\":[\"l\"]}");
+
+    runAddCommand(tagFilename, viewFilename);
+    runCommitAllCommand();
+    runCheckoutCommand(viewName);
+
+    boolean success = cli.run(command, "transcription");
+    assertSucceedsWithExpectedStdout(success, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+        "<xml>\n" +
+        "<l>test</l>\n" +
+        "</xml>");
+  }
+
+  @Test
+  public void testCommandHelp() throws Exception {
+    final boolean success = cli.run(command, "-h");
+    assertSucceedsWithExpectedStdout(success, "usage: java -jar alexandria-app.jar\n" +
+        "       export-xml [-h] DOCUMENT\n" +
+        "\n" +
+        "Export the document as xml.\n" +
+        "\n" +
+        "positional arguments:\n" +
+        "  DOCUMENT               The name of the document to export.\n" +
+        "\n" +
+        "named arguments:\n" +
+        "  -h, --help             show this help message and exit");
+  }
+
+  @Test
+  public void testCommandShouldBeRunInAnInitializedDirectory() throws Exception {
+    assertCommandRunsInAnInitializedDirectory(command, "document");
+  }
+}
--- alexandria-markup-server/src/main/java/nl/knaw/huygens/alexandria/dropwizard/cli/commands/ExportXmlCommand.java
+++ alexandria-markup-server/src/main/java/nl/knaw/huygens/alexandria/dropwizard/cli/commands/ExportXmlCommand.java
@@ -20,76 +20,51 @@ package nl.knaw.huygens.alexandria.dropwizard.cli.commands;
  * #L%
  */
 
-import com.google.common.base.Charsets;
 import io.dropwizard.setup.Bootstrap;
 import net.sourceforge.argparse4j.inf.Namespace;
 import net.sourceforge.argparse4j.inf.Subparser;
 import nl.knaw.huc.di.tag.TAGViews;
 import nl.knaw.huc.di.tag.tagml.xml.exporter.XMLExporter;
+import nl.knaw.huygens.alexandria.dropwizard.cli.CLIContext;
 import nl.knaw.huygens.alexandria.storage.TAGDocument;
 import nl.knaw.huygens.alexandria.storage.TAGStore;
 import nl.knaw.huygens.alexandria.view.TAGView;
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-import java.io.IOException;
 
 public class ExportXmlCommand extends AlexandriaCommand {
   private static final String DOCUMENT = "document";
   private static final String VIEW = "view";
 
   public ExportXmlCommand() {
-    super("export-xml", "Export (the optional view of) the document as xml.");
+    super("export-xml", "Export the document as xml.");
   }
 
   @Override
   public void configure(Subparser subparser) {
-    subparser.addArgument("-d", "--document")//
+    subparser.addArgument("document")//
         .dest(DOCUMENT)//
+        .metavar("DOCUMENT")
         .type(String.class)//
         .required(true)//
         .help("The name of the document to export.");
-    subparser.addArgument("-v", "--view")//
-        .dest(VIEW)
-        .type(String.class)//
-        .required(false)//
-        .help("The name of the view to use.");
   }
 
   @Override
   public void run(Bootstrap<?> bootstrap, Namespace namespace) {
     checkDirectoryIsInitialized();
+
     String docName = namespace.getString(DOCUMENT);
-    String viewName = namespace.getString(VIEW);
-    boolean useView = viewName != null;
     Long docId = getIdForExistingDocument(docName);
     try (TAGStore store = getTAGStore()) {
       store.runInTransaction(() -> {
-        System.out.printf("document: %s%n", docName);
-        System.out.printf("Retrieving document %s%n", docName);
+        CLIContext context = readContext();
+        String viewName = context.getActiveView();
+        TAGView tagView = MAIN_VIEW.equals(viewName)
+            ? TAGViews.getShowAllMarkupView(store)
+            : getExistingView(viewName, store, context);
         TAGDocument document = store.getDocument(docId);
-        String format = "xml";
-
-        TAGView tagView;
-        if (useView) {
-          System.out.printf("Retrieving view %s%n", viewName);
-          tagView = getExistingView(viewName, store, readContext());
-        } else {
-          tagView = TAGViews.getShowAllMarkupView(store);
-        }
-
-        String sub = useView ? "-" + viewName : "";
-        String fileName = String.format("%s%s.%s", docName, sub, format);
-        System.out.printf("exporting to file %s...", fileName);
-        try {
-          XMLExporter xmlExporter = new XMLExporter(store, tagView);
-          String xml = xmlExporter.asXML(document);
-          FileUtils.writeStringToFile(new File(fileName), xml, Charsets.UTF_8);
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-        System.out.println();
-        System.out.println("done!");
+        XMLExporter xmlExporter = new XMLExporter(store, tagView);
+        String xml = xmlExporter.asXML(document);
+        System.out.println(xml);
       });
     }
   }
