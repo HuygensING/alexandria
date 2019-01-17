738a6826c2ebc015f57d08d0e69cdf19430a385e
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2019-01-17 10:33:13 +0100

    remove unused import statement

--- alexandria-markup-client/src/test/java/nl/knaw/huygens/alexandria/dropwizard/cli/StatusCommandIntegrationTest.java
+++ alexandria-markup-client/src/test/java/nl/knaw/huygens/alexandria/dropwizard/cli/StatusCommandIntegrationTest.java
@@ -4,7 +4,7 @@ package nl.knaw.huygens.alexandria.dropwizard.cli;
  * #%L
  * alexandria-markup-client
  * =======
- * Copyright (C) 2015 - 2018 Huygens ING (KNAW)
+ * Copyright (C) 2015 - 2019 Huygens ING (KNAW)
  * =======
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,6 @@ import org.junit.Test;
 import java.nio.file.Files;
 import java.time.Instant;
 
-import static junit.framework.TestCase.fail;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StatusCommandIntegrationTest extends CommandIntegrationTest {
