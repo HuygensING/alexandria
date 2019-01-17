b884c962cb607cb697fc16098431c584020fcc0e
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2019-01-17 10:34:06 +0100

    update copyright headers

--- alexandria-markup-client/src/main/java/nl/knaw/huygens/alexandria/markup/client/RestResult.java
+++ alexandria-markup-client/src/main/java/nl/knaw/huygens/alexandria/markup/client/RestResult.java
@@ -6,7 +6,7 @@ import java.time.Duration;
  * #%L
  * alexandria-markup-client
  * =======
- * Copyright (C) 2015 - 2018 Huygens ING (KNAW)
+ * Copyright (C) 2015 - 2019 Huygens ING (KNAW)
  * =======
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
