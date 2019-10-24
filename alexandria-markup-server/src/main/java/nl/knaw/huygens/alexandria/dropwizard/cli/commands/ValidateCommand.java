b3ee58c8326d0ff016301f0dea002aaaf1ae564c
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2019-10-24 18:24:13 +0200

    [TRD-428] Setup validate command

---
+++ alexandria-markup-server/src/main/java/nl/knaw/huygens/alexandria/dropwizard/cli/commands/ValidateCommand.java
@@ -0,0 +1,64 @@
+package nl.knaw.huygens.alexandria.dropwizard.cli.commands;
+
+/*
+ * #%L
+ * alexandria-markup-server
+ * =======
+ * Copyright (C) 2015 - 2019 Huygens ING (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import io.dropwizard.setup.Bootstrap;
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
+import nl.knaw.huygens.alexandria.dropwizard.cli.CLIContext;
+import nl.knaw.huygens.alexandria.storage.TAGStore;
+
+import java.io.IOException;
+
+public class ValidateCommand extends AlexandriaCommand {
+
+  private static final String SCHEMA = "schema";
+
+  public ValidateCommand() {
+    super("validate", "Validate a document agains a TAG schema.");
+  }
+
+  @Override
+  public void configure(Subparser subparser) {
+    subparser
+        .addArgument("-d", "--document") //
+        .dest(DOCUMENT) //
+        .type(String.class) //
+        .required(true) //
+        .help("The name of the document to validate.");
+    subparser
+        .addArgument("-s", "--schema") //
+        .dest(SCHEMA)
+        .type(String.class) //
+        .required(true) //
+        .help("The TAG schema file.");
+  }
+
+  @Override
+  public void run(Bootstrap<?> bootstrap, Namespace namespace) throws IOException {
+    checkAlexandriaIsInitialized();
+
+    CLIContext context = readContext();
+
+    try (TAGStore store = getTAGStore()) {}
+    System.out.println();
+  }
+}
