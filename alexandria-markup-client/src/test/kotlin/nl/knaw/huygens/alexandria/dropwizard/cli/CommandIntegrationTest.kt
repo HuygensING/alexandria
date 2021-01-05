c2946c6ca9c160880fa58bed800fbde507c92e78
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-01-05 10:48:50 +0100

    update copyright headers

--- alexandria-markup-client/src/test/kotlin/nl/knaw/huygens/alexandria/dropwizard/cli/CommandIntegrationTest.kt
+++ alexandria-markup-client/src/test/kotlin/nl/knaw/huygens/alexandria/dropwizard/cli/CommandIntegrationTest.kt
@@ -3,7 +3,7 @@ package nl.knaw.huygens.alexandria.dropwizard.cli
  * #%L
  * alexandria-markup-client
  * =======
- * Copyright (C) 2015 - 2020 Huygens ING (KNAW)
+ * Copyright (C) 2015 - 2021 Huygens ING (KNAW)
  * =======
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
